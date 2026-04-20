0afe5891fc3a625ba0e74ab9d267b74935eb9c73
Jigar Rathod <jigar@solvios.technology>
2026-04-20 15:38:55 +0530

    js update

--- category-new.js
+++ category-new.js
@@ -49,22 +49,28 @@
 //   });
 // });
 
-jQuery(document).ready(function ($) {
-  $(".faq-answer").hide();
+document.addEventListener("DOMContentLoaded", function () {
+  const itemsfaq = document.querySelectorAll(".faq-item");
 
-  $(".faq-question").on("click", function () {
-    var $parent = $(this).closest(".faq-item");
-    var $icon = $(this).find(".icon-img");
-    var $answer = $parent.find(".faq-answer");
+  itemsfaq.forEach((item) => {
+    item.querySelector(".faq-question").addEventListener("click", () => {
+      const isActive = item.classList.contains("active");
 
-    $answer.slideToggle();
+      // Close all
+      itemsfaq.forEach((el) => {
+        el.classList.remove("active");
 
-    if ($parent.hasClass("active")) {
-      $parent.removeClass("active");
-      $icon.attr("src", themeData.themeUrl + "/img/plus-icon.svg");
-    } else {
-      $parent.addClass("active");
-      $icon.attr("src", themeData.themeUrl + "/img/minus-icon.svg");
-    }
+        const img = el.querySelector(".icon-img");
+        img.src = img.dataset.plus; // ✅ dynamic
+      });
+
+      // Open current
+      if (!isActive) {
+        item.classList.add("active");
+
+        const img = item.querySelector(".icon-img");
+        img.src = img.dataset.minus; // ✅ dynamic
+      }
+    });
   });
 });
